f4927483eca6d6a911da699bb30bab3912cb3ae6
austinbernard21 <35849483+austinbernard21@users.noreply.github.com>
2018-04-27 19:58:28 -0700

    Add files via upload
    functional version with specified coordinates when phone is screen mirrored. added screenshot function and removed image manipulation.

--- hq_script.py
+++ hq_script.py
@@ -9,7 +9,8 @@ import pytesseract
 import numpy
 import cv2
 import PIL
-from PIL import Image
+import argparse
+from PIL import Image, ImageEnhance, ImageFilter
 import time
 
 pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\Austin\\Tesseract-OCR\\tesseract.exe"
@@ -28,19 +29,27 @@ def grab_text(url_list):
         html = urlopen(url_list[1])
         return html
     
+def screenshot():
+    pic = pyautogui.screenshot(region=(23,216,380,376))
+    image = pic.save('Screenshot.jpg')
+    image = Image.open('Screenshot.jpg')
+    return image
 
-def grab_image():
-    question_interface = numpy.array(PIL.ImageGrab.grab(bbox=(343,367,682,682))) #650,218,946,500
-    question_interface = cv2.cvtColor(question_interface,cv2.COLOR_BGR2RGB)
-    question_interface = cv2.resize(question_interface, (0,0),fx=2,fy=2) #3,3
-    ret,question_interface = cv2.threshold(question_interface,127,255,cv2.THRESH_BINARY)
-    question_interface = PIL.Image.fromarray(question_interface,'RGB')
-    return pytesseract.image_to_string(question_interface)
+#def grab_image():
+    #image = PIL.Image.open('Screenshot.jpg').convert("L")
+    #question_interface = numpy.array(image)
+    #question_interface = numpy.array(PIL.ImageGrab.grab(bbox=(23,222,402,581))) #25,185,420,570
+    #question_interface = cv2.cvtColor(question_interface,cv2.COLOR_BGR2RGB)
+    #question_interface = cv2.resize(question_interface, (0,0),fx=2,fy=2) #3,3
+    #ret,question_interface = cv2.threshold(question_interface,127,255,cv2.THRESH_BINARY)
+    #image = PIL.Image.fromarray(question_interface,'RGB')
+    #return pytesseract.image_to_string(question_interface)
 
 def break_ans_ques(text):
     first_break = text.find("\n\n")
     question = text[0:first_break+1]
     text = text[first_break+2:].replace("\n\n","\n")
+    print(text)
     ans = text.split("\n")
     if len(ans) > 3 :
         question = question + " " + ans[0]
@@ -70,13 +79,14 @@ def result_analysis_1(answers):
 def print_time():
     millis = int(round(time.time()*1000))
     print(millis)
-    
+
 
 if __name__ == '__main__':
     print("start of program")
     print_time()
-    all_text = grab_image()
-    print(all_text)
+    image = screenshot()
+    all_text = pytesseract.image_to_string(image)
+    #all_text = grab_image()
     print("after grab image: "),
     print_time()
     all_text = break_ans_ques(all_text)
@@ -84,8 +94,8 @@ if __name__ == '__main__':
     print_time()
     query = all_text[0]
     url_list = list(search_web(query))
-    for link in url_list:
-        print(link)
+    #for link in url_list:
+        #print(link)
     print("after search_web: "),
     print_time()
     sample_ans_1 = all_text[1]
@@ -101,6 +111,8 @@ if __name__ == '__main__':
     print("after html cleaning up: "),
     print_time()
     answers = [sample_ans_1,sample_ans_2,sample_ans_3]
+
+    print("\n\n\n")
 
     result_analysis_1(answers)
     print("after analysis: "),
