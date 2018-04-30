7dd308b88e2d51105593b7872dcfe011e32301ed
austinbernard21 <35849483+austinbernard21@users.noreply.github.com>
2018-04-29 18:20:15 -0700

    Add files via upload

--- hq_script.py
+++ hq_script.py
@@ -9,7 +9,6 @@ import pytesseract
 import numpy
 import cv2
 import PIL
-import argparse
 from PIL import Image, ImageEnhance, ImageFilter
 import time
 
@@ -29,21 +28,18 @@ def grab_text(url_list):
         html = urlopen(url_list[1])
         return html
     
-def screenshot():
-    pic = pyautogui.screenshot(region=(23,216,380,376))
-    image = pic.save('Screenshot.jpg')
-    image = Image.open('Screenshot.jpg')
-    return image
+#def screenshot():
+    #pic = pyautogui.screenshot(region=(23,216,380,376))
+    #image = pic.save('Screenshot.jpg')
+    #image = Image.open('Screenshot.jpg')
+    #return image
 
-#def grab_image():
-    #image = PIL.Image.open('Screenshot.jpg').convert("L")
-    #question_interface = numpy.array(image)
-    #question_interface = numpy.array(PIL.ImageGrab.grab(bbox=(23,222,402,581))) #25,185,420,570
-    #question_interface = cv2.cvtColor(question_interface,cv2.COLOR_BGR2RGB)
-    #question_interface = cv2.resize(question_interface, (0,0),fx=2,fy=2) #3,3
-    #ret,question_interface = cv2.threshold(question_interface,127,255,cv2.THRESH_BINARY)
-    #image = PIL.Image.fromarray(question_interface,'RGB')
-    #return pytesseract.image_to_string(question_interface)
+def grab_image():
+    question_interface = numpy.array(PIL.ImageGrab.grab(bbox=(23,215,402,582))) #25,185,420,570
+    question_interface = cv2.cvtColor(question_interface,cv2.COLOR_BGR2RGB)
+    question_interface = cv2.resize(question_interface, (0,0),fx=2,fy=2) #3,3
+    ret,question_interface = cv2.threshold(question_interface,127,255,cv2.THRESH_BINARY)
+    return pytesseract.image_to_string(question_interface)
 
 def break_ans_ques(text):
     first_break = text.find("\n\n")
@@ -84,9 +80,9 @@ def print_time():
 if __name__ == '__main__':
     print("start of program")
     print_time()
-    image = screenshot()
-    all_text = pytesseract.image_to_string(image)
-    #all_text = grab_image()
+    #image = screenshot()
+    #all_text = pytesseract.image_to_string(image)
+    all_text = grab_image()
     print("after grab image: "),
     print_time()
     all_text = break_ans_ques(all_text)
@@ -94,13 +90,16 @@ if __name__ == '__main__':
     print_time()
     query = all_text[0]
     url_list = list(search_web(query))
-    #for link in url_list:
-        #print(link)
+    for link in url_list:
+        if "quora" in link:
+            url_list.remove(link)
+    for link in url_list:
+        print(link)
     print("after search_web: "),
     print_time()
-    sample_ans_1 = all_text[1]
-    sample_ans_2 = all_text[2]
-    sample_ans_3 = all_text[3]
+    sample_ans_1 = all_text[1].replace(".","")
+    sample_ans_2 = all_text[2].replace(".","")
+    sample_ans_3 = all_text[3].replace(".","")
     page_html = (grab_text(url_list).read())
     print("after reading url: "),
     print_time()
@@ -115,8 +114,6 @@ if __name__ == '__main__':
     print("\n\n\n")
 
     result_analysis_1(answers)
-    print("after analysis: "),
-    print_time()
-    
-    
+    #print("after analysis: "),
+    #print_time()
     
